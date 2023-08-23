0bc214c1203547472d20dcd0acc15ae0d62c38c5
Anshul Halwa <anshulhalwa91@gmail.com>
2023-08-23 22:43:19 +0530

    Redux_axios_rest_API

---
+++ src/containers/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="ui fixed menu">
+            <div className="ui container center">
+                <h2>Clothing Shop</h2>
+            </div>
+        </div>
